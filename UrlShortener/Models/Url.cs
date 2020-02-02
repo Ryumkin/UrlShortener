746c727393883b61dc88426eec3085eaf3d4979a
Svyatoslav <svyat94@mail.ru>
2020-02-02 19:47:47 +0300

    initial migrations

---
+++ UrlShortener/Models/Url.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Models
+{
+    public class Url
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdHash { get; set; }
+
+        [Required]
+        public string LongUl { get; set; }
+    }
+}
